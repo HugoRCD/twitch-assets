0504b2a8d879dc08a0b0c9fb75a4476538c331cc
HugoRCD <hugo.richard@epitech.eu>
2025-05-19 21:17:33 +0200

    up

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <UApp>
+    Hello twitch
+  </UApp>
+</template>
---
+++ app/components/Player.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  test: string
+}>()
+</script>
+
+<template>
+  <div>
+    Hello {{ test }}
+  </div>
+</template>
