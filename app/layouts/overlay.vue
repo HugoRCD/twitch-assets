a764bc7d63c69a224420488fa50fc811dfc17f90
HugoRCD <hugo.richard@epitech.eu>
2025-05-22 23:38:45 +0200

    up

--- app/layouts/overlay.vue
+++ app/layouts/overlay.vue
@@ -6,8 +6,8 @@ const { stream } = useStream()
   <UApp class="relative">
     <div class="w-8 h-full absolute top-0 left-0 bg-default" />
     <div class="w-8 h-full absolute top-0 right-0 bg-default" />
-    <div class="w-8 h-full absolute bottom-0 left-0 bg-default" />
-    <div class="w-8 h-full absolute bottom-0 right-0 bg-default" />
+    <div class="w-full h-8 absolute bottom-0 left-0 top-0 bg-default" />
+    <div class="w-full h-8 absolute bottom-0 right-0 left-0 bg-default" />
     <div class="flex min-h-screen flex-col p-8 z-20">
       <div class="relative flex size-full mx-auto flex-1 flex-col gap-3 border-2 border-default">
         <NbViewer :nb-viewers="stream?.stream?.viewer_count" class="absolute top-2 right-2" />
